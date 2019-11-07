4c9d5ea85c6c664b06f8b951147574852400f874
Alan Mimms <alanmimms@gmail.com>
2019-11-06 17:13:45 -0800

    Add some more utilities to make bitfields easier to manipulate
    Former-commit-id: 15e6d65f9fdea9370263f2c2c2b5ed3ab62aff0f

--- util.js
+++ util.js
@@ -1,8 +1,10 @@
+'use strict';
+
 // For a BigInt parameter `n`, return its value decoded as groups of
 // four octal digits with leading zeroes. For Number `n`, just return
 // leading zero octal digits for the specified bits-per-word size
 // `bpw`.
-module.exports.octal4 = function octal4(n, bpw = 12) {
+function octal4(n, bpw = 12) {
 
   if (typeof n === 'bigint') {
     const oneMore = 1n << BigInt(bpw);
@@ -12,3 +14,46 @@ module.exports.octal4 = function octal4(n, bpw = 12) {
     return (n | oneMore).toString(8).slice(1);
   }
 }
+module.exports.octal4 = octal4;
+
+
+// Return BigInt bit mask for PDP bit numbering bit `n` in word of
+// width `w`.
+function maskForBit(n, w = 36) {
+  return BigInt(Math.pow(2, w - 1 - n));
+}
+module.exports.maskForBit = maskForBit;
+
+
+// In a PDP10 word of `w` bits, return the shift right count to get
+// bit #n into LSB.
+function shiftForBit(n, w = 36) {
+  return BigInt(w) - 1n - BigInt(n);
+}
+module.exports.shiftForBit = shiftForBit;
+
+
+// Return a BigInt mask for PDP10 numbered bit positions within `w` bit word
+// of `s`..`e`. 
+function fieldMask(s, e, w) {
+  const sMask = (1n << shiftForBit(s - 1, w)) - 1n;
+  const rightMask = (1n << shiftForBit(e, w)) - 1n;
+  return sMask ^ rightMask;
+}
+module.exports.fieldMask = fieldMask;
+
+
+// Insert into `v` whose full width is `w` bits the value `n` at PDP10
+// numbered bit positions within `v` of `s`..`e`. All values are BigInt.
+function fieldInsert(v, n, s, e, w) {
+  return v & ~fieldMask(s, e, w) | (BigInt(n) << shiftForBit(e, w));
+}
+module.exports.fieldInsert = fieldInsert;
+
+
+// Return from `v` whose full width is `w` bits the PDP10 numbered
+// bitfield `s`..`e`. All values are BigInt.
+function fieldExtract(v, s, e, w) {
+  return (v & fieldMask(s, e, w)) >> shiftForBit(e, w);
+}
+module.exports.fieldExtract = fieldExtract;
