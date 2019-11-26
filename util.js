926cad3b61591992190d1a0e69548077b6f7d89a
Alan Mimms <alanmimms@gmail.com>
2019-11-26 12:32:35 -0800

    Cleanup
    Former-commit-id: 294540df58d0b4386884adf32158095ddf642007

--- util.js
+++ util.js
@@ -22,7 +22,7 @@ const octW = module.exports.octW = n => octal(n, 12, 6, ',,');
 // Return BigInt bit mask for PDP bit numbering bit `n` in word of
 // width `w`.
 function maskForBit(n, w = 36) {
-  return BigInt(Math.pow(2, w - 1 - n));
+  return 1n << shiftForBit(n, w);
 }
 module.exports.maskForBit = maskForBit;
 
@@ -30,32 +30,33 @@ module.exports.maskForBit = maskForBit;
 // In a PDP10 word of `w` bits, return the shift right count to get
 // bit #n into LSB.
 function shiftForBit(n, w = 36) {
-  return BigInt(w) - 1n - BigInt(n);
+  return BigInt(w - 1 - n);
 }
 module.exports.shiftForBit = shiftForBit;
 
 
 // Return a BigInt mask for PDP10 numbered bit positions within `w` bit word
 // of `s`..`e`. 
-function fieldMask(s, e, w) {
-  const sMask = (1n << shiftForBit(s - 1, w)) - 1n;
-  const rightMask = (1n << shiftForBit(e, w)) - 1n;
-  return sMask ^ rightMask;
+function fieldMask(s, e, w = 36) {
+  const sMask = maskForBit(s - 1, w) - 1n;
+  const rightMask = maskForBit(e, w) - 1n;
+  return sMask - rightMask;
 }
 module.exports.fieldMask = fieldMask;
 
 
-// Insert into `v` whose full width is `w` bits the value `n` at PDP10
-// numbered bit positions within `v` of `s`..`e`. All values are BigInt.
-function fieldInsert(v, n, s, e, w) {
+// Insert into BigInt `v` whose full width is `w` bits the value `n`
+// at PDP10 numbered bit positions within `v` of `s`..`e`. Returns
+// BigInt.
+function fieldInsert(v, n, s, e, w = 36) {
   return v & ~fieldMask(s, e, w) | (BigInt(n) << shiftForBit(e, w));
 }
 module.exports.fieldInsert = fieldInsert;
 
 
-// Return from `v` whose full width is `w` bits the PDP10 numbered
-// bitfield `s`..`e`. All values are BigInt.
-function fieldExtract(v, s, e, w) {
+// Return BigInt extracted from BigInt `v` whose full width is `w`
+// bits the PDP10 numbered bitfield `s`..`e`.
+function fieldExtract(v, s, e, w = 36) {
   return (v & fieldMask(s, e, w)) >> shiftForBit(e, w);
 }
 module.exports.fieldExtract = fieldExtract;
