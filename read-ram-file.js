e80b236a66ba1161b48f5a8164e2c1472e075bd0
Alan Mimms <alanmimms@gmail.com>
2019-12-31 11:25:58 -0800

    Prepare to read EBOX diagnostic microcode from .RAM files for testing

--- read-ram-file.js
+++ read-ram-file.js
@@ -1,12 +1,13 @@
 'use strict';
 const fs = require('fs');
 const util = require('util');
+const {octal,oct6} = require('./util');
 
 
 function decode(s) {
 
   return s.split('').reduce((sum, cur) => {
-    const ch = cur.charCodeAt(0);
+    let ch = cur.charCodeAt(0);
 
     if (ch >= 0o100) ch -= 0o100;
     return (sum << 6) | ch;
@@ -18,16 +19,43 @@ function dump(lines) {
 
   lines.forEach(line => {
     const recType = line[0];
+    if (recType === undefined || recType === ';') return;
+
     const dataS = line.slice(2);
-    const data = dataS.split(/,/).map(decode);
+    const [wc, adr, ...data] = dataS.split(/,/).map(decode);
+    const checksum = data.pop();
+
+    switch(recType) {
+    case 'Z':
+      const count = data[0];
+      console.log(`Zero ${wc}@${octal(adr)} count=${count}`);
+      break;
+
+    case 'C':
+    case 'D':
+      const dataS = data.map((w, x) => oct6(w) + (x % 6 === 5 ? '\n            ' : '')).join(',');
+      console.log(`${recType === 'C' ? 'CRAM' : 'DRAM'} ${wc}@${octal(adr)}:${dataS}`);
+      break;
+
+    default:
+      console.log(`Unknown recType: "${recType}"`);
+      break;
+    }
   });
 }
 
 
 function main() {
-  const eboxaS = fs.readFileSync('kl10-source/eboxa.ram').toString().split(/\n/);
-  dump(eboxaS);
+  const eboxa = fs.readFileSync('kl10-source/eboxa.ram').toString().split(/\n/);
+  console.log(`
+EBOX A:`);
+  dump(eboxa);
 
-  const eboxbS = fs.readFileSync('kl10-source/eboxb.ram').toString().split(/\n/);
-  dump(eboxbS);
+  const eboxb = fs.readFileSync('kl10-source/eboxb.ram').toString().split(/\n/);
+  console.log(`
+EBOX B:`);
+  dump(eboxb);
 }
+
+
+main();
